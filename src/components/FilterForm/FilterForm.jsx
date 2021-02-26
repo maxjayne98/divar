0185aba623551b2fbc9a86c44d7a332825d70c4b
mahdi <mahdi.jax@gmail.com>
2021-02-26 23:48:06 +0330

    some change to add reusability to form and input

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchInput from "../SearchInput";
 import "./FilterForm.css";
 function FilterForm() {
+  const [formValues, setFormValues] = useState({
+    field: "",
+    name: "",
+    date: "",
+    title: "",
+  });
+
+  const changeHandler = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  function formSubmit(ev) {
+    console.log(formValues);
+    ev.preventDefault();
+  }
   return (
-    <div className="filter-form">
-      <SearchInput name="field" type="text" />
-      <SearchInput name="name" type="text" />
-      <SearchInput name="date" type="date" />
-      <SearchInput name="title" type="text" />
-    </div>
+    <form className="filter-form" onSubmit={formSubmit}>
+      <SearchInput name="field" type="text" handleOnChange={changeHandler} />
+      <SearchInput name="name" type="text" handleOnChange={changeHandler} />
+      <SearchInput name="date" type="date" handleOnChange={changeHandler} />
+      <SearchInput name="title" type="text" handleOnChange={changeHandler} />
+      <SearchInput name="submit" type="submit" />
+    </form>
   );
 }
 export default FilterForm;
--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,19 +1,14 @@
 import React, { useState } from "react";
 import "./SearchInput.css";
 
-function SearchInput({ name, type }) {
-  const [value, setValue] = useState("");
-  function onChangeInput(ev) {
-    setValue(ev.target.value);
-    console.log(ev.target.value);
-  }
+function SearchInput({ name, type, handleOnChange }) {
   return (
     <input
       className="search-input"
       type={type}
-      value={value}
-      onChange={onChangeInput}
+      onChange={handleOnChange}
       placeholder={name}
+      name={name}
     />
   );
 }
